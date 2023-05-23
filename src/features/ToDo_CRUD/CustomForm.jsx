ae162f5c9d0b085cde4d72b49b4afd0c8b2a4cea
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-05-23 21:39:02 +0200

    Connect sass to components

---
+++ src/features/ToDo_CRUD/CustomForm.jsx
@@ -0,0 +1,11 @@
+function CustomForm() {
+  return (
+    <div className="CustomForm">
+      <form className="todo">
+
+      </form>
+    </div>
+  )
+}
+
+export default CustomForm
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CustomForm from './features/ToDo_CRUD/CustomForm';
+import "./assets/sass/main.scss";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <CustomForm />
   </React.StrictMode>,
 )
